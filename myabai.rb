2178a5a71adb49d6a3ac6433fc2f910ff44ad930
Mikkel Ricky <rimi@aarhus.dk>
2023-12-29 11:47:55 +0100

    Add myabai

---
+++ myabai.rb
@@ -0,0 +1,45 @@
+class Myabai < Formula
+  desc "A tiling window manager for macOS based on binary space partitioning."
+  homepage "https://github.com/mikkelricky/yabai"
+  head "https://github.com/mikkelricky/yabai.git", branch: "grid-spaces"
+
+  depends_on :macos => :big_sur
+
+  def install
+    man.mkpath
+
+    if build.head?
+      system "make", "-j1", "install"
+    end
+
+    bin.install "#{buildpath}/bin/yabai"
+    (pkgshare/"examples").install "#{buildpath}/examples/yabairc"
+    (pkgshare/"examples").install "#{buildpath}/examples/skhdrc"
+    man1.install "#{buildpath}/doc/yabai.1"
+  end
+
+  def caveats; <<~EOS
+    Copy the example configuration into your home directory:
+      cp #{opt_pkgshare}/examples/yabairc ~/.yabairc
+      cp #{opt_pkgshare}/examples/skhdrc ~/.skhdrc
+
+    If you want yabai to be managed by launchd (start automatically upon login):
+      yabai --start-service
+
+    When running as a launchd service logs will be found in:
+      /tmp/yabai_<user>.[out|err].log
+
+    If you are using the scripting-addition; remember to update your sudoers file:
+      sudo visudo -f /private/etc/sudoers.d/yabai
+
+    Build the configuration row by running:
+      echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | cut -d " " -f 1) $(which yabai) --load-sa"
+
+    README: https://github.com/koekeishiya/yabai/wiki/Installing-yabai-(latest-release)#configure-scripting-addition
+    EOS
+  end
+
+  test do
+    assert_match "yabai-v#{version}", shell_output("#{bin}/yabai --version")
+  end
+end
